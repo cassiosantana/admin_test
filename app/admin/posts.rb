62a4815c8e9e5b4e598187908e707035d66f3f28
Cassio Santana <cassioo.roger@gmail.com>
2023-08-29 19:35:25 -0300

    feat: Define post columns

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Post do
-  permit_params :title, :body
+  permit_params :title, :content
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :content
+    actions
+  end
 end
